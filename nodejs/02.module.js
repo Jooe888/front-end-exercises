e7d804e2339ab88c9ab13f15242a35160a71ddf2
Jooe <ojooeo@gmail.com>
2020-12-14 14:39:51 +0800

    module.export 导出

--- nodejs/02.module.js
+++ nodejs/02.module.js
@@ -6,8 +6,24 @@
 在 Node 中，每一个 js 文件中的 js 代码都是独立运行在一个函数中，而不是全局作用域，所以一个模块中的变量和函数在其他模块中无法访问
 */
 
-console.log('我是一个模块');
+// console.log('我是一个模块');
 
-const md = require('./01.hellonode');
+// const md = require('./01.hellonode');
 
-console.log(md.x, md.addition(1, 2));
+// console.log(md.x, md.addition(1, 2));
+
+// 这样导出太麻烦了， 每次都得写 exports
+// exports.name = '孙悟空';
+// exports.age = 18;
+// exports.sayInfo = () => {
+//   console.log('我是孙悟空～～～～～');
+// };
+
+// 可以这么导出
+module.exports = {
+  name: '猪八戒',
+  age: 16,
+  sayName: info => {
+    console.log(info);
+  }
+};
---
+++ nodejs/03.helloModule.js
@@ -0,0 +1,2 @@
+const hello = require('./02.module');
+hello.sayName(hello.age);
