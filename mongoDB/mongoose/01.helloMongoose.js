e8df57d9d96d70a51737bc011bd40a8afee095bc
Jooe <ojooeo@gmail.com>
2020-12-26 23:28:07 +0800

    mongoose

---
+++ mongoDB/mongoose/01.helloMongoose.js
@@ -0,0 +1,8 @@
+/*  
+1. 下载安装 Mongoose
+  - npm install mongoose --save
+2. 在项目中引入 mongoose
+  - const mongoose = require('mongoose');
+3. 连接 MongoDB 数据库
+  - mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
+*/
---
+++ mongoDB/mongoose/mongoose.md
@@ -0,0 +1,13 @@
+# Mongoose 中为我们提供了新的对象
+
+## Schema (模式对象)
+
+- schema 对象定义了约束数据库中的文档结构
+
+## Model
+
+- Model 对象作为集合中的所有文档的表示，相当于 MongoDB 数据库中的集合 collection
+
+## Document
+
+- Document 表示集合中的具体文档，相当于集合中的一个具体的文档
