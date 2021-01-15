6190ebe52cdafd43b478eb31506f60ff0a70f02c
Jooe <ojooeo@gmail.com>
2021-01-15 15:57:29 +0800

    tsconfig

--- TypeScript/part2/index.ts
+++ TypeScript/part2/index.ts
@@ -1,1 +1,3 @@
 console.log('hello');
+console.log(1111);
+// 这是一个注释
---
+++ TypeScript/part2/src/app.ts
@@ -0,0 +1,10 @@
+// import { hi } from './hello/hello';
+let a: string = 'b';
+let obj = {
+  a: 1,
+  b: 2
+};
+
+let c: unknown = obj?.a;
+console.log(c);
+// console.log(hi);
