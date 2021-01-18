cc0a3873215f25b3b80c8f3c7bb754fbfb16f3ea
Jooe <ojooeo@gmail.com>
2021-01-18 15:05:24 +0800

    .

--- TypeScript/part3/src/index.ts
+++ TypeScript/part3/src/index.ts
@@ -1,2 +1,12 @@
 let a: string = '';
 a = 'hello';
+
+const obj = {
+  name: '孙悟空',
+  age: 16
+};
+obj.age = 18;
+
+console.log(obj);
+
+console.log(Promise);
--- TypeScript/part3/webpack.config.js
+++ TypeScript/part3/webpack.config.js
@@ -1,6 +1,9 @@
 // 引入一个包
 const path = require('path');
-
+// 引入 html 插件
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+// 引入 clean 插件
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 // webpack 中的所有的配置信息都应该写在 module.exports 中
 module.exports = {
   // 指定入口文件
@@ -10,9 +13,13 @@ module.exports = {
     // 指定打包文件的目录
     path: path.resolve(__dirname, 'dist'),
     // 打包后文件的文件
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    // 告诉 webpack 不实用箭头函数
+    environment: {
+      arrowFunction: false
+    }
   },
-
+  mode: 'production',
   // 指定 webpack 打包时要使用的模块
   module: {
     // 指定要加载的规则
@@ -21,10 +28,45 @@ module.exports = {
         // test 指定的是规则生效的文件
         test: /\.ts$/,
         // 要使用的 loader
-        use: 'ts-loader',
+        use: [
+          // 配置babel
+          {
+            // 指定加载器
+            loader: 'babel-loader',
+            // 设置babel
+            options: {
+              // 设置定义的环境
+              presets: [
+                [
+                  // 指定环境的插件
+                  '@babel/preset-env',
+                  // 配置信息
+                  {
+                    // 要兼容的目标浏览器
+                    targets: {
+                      chrome: '88',
+                      ie: '11'
+                    },
+                    // 指定 corejs 的版本
+                    corejs: '3',
+                    // 使用 corejs 的方法 "usage" 表示按需加载
+                    useBuiltIns: 'usage'
+                  }
+                ]
+              ]
+            }
+          },
+          'ts-loader'
+        ],
         // 要排除的文件
         exclude: /node_modules/
       }
     ]
-  }
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin()
+  ]
 };
