2fb52f89761cd66fb7450bab360e00ae3fa3c106
Jooe <ojooeo@gmail.com>
2021-01-25 14:08:54 +0800

    继承

--- TypeScript/chapter02/part1/src/03.继承.ts
+++ TypeScript/chapter02/part1/src/03.继承.ts
@@ -1,6 +1,6 @@
 (() => {
-  // 定义一个表示狗的类
-  class Dog {
+  // 定义一个 Animal 类
+  class Animal {
     name: string;
     age: number;
     // constructor 构造函数
@@ -16,13 +16,28 @@
 
     bark() {
       // 在方法在是可以通过 this 来表示当前调用方法的对象
-      console.log(this.name);
+      console.log(`${this.name}在叫`);
     }
   }
 
+  // 定义一个表示狗的类
+  // 使 Dog 类继承 Animal 类
+  /*  
+    Dog extends Animal
+      - 此时，Animal 被称为父类，Dog 被称为子类
+      - 使用继承后，之类将会拥有父类所有的方法和属性
+      - 通过继承可以将多个类中共有的代码写在一个父类中，
+          这样只需要写一次即可让所有的之类都同时拥有父类中的属性 
+  */
+  class Dog extends Animal {}
+
+  class Cat extends Animal {}
+
   const dog = new Dog('小黑', 5);
+  const cat = new Cat('咪咪', 3);
 
   console.log(dog);
-
+  console.log(cat);
   dog.bark();
+  cat.bark();
 })();
