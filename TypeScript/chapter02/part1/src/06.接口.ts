d20064194fb71d662dbac4f381c6786b3a25bb8b
Jooe <ojooeo@gmail.com>
2021-01-28 16:15:46 +0800

    abstract

---
+++ TypeScript/chapter02/part1/src/06.接口.ts
@@ -0,0 +1,15 @@
+(() => {
+  // 描述一个对象的类型
+  type myType = {
+    name: string;
+    age: number;
+  };
+  const obj: myType = {
+    name: 'sss',
+    age: 1
+  };
+
+  /*
+    接口就是用来定义一个类的结构
+  */
+})();
