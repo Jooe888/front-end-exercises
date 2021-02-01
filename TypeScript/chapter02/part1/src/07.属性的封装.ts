3449139baabb6db8d58e6a89054b4a247b02baf5
Jooe <ojooeo@gmail.com>
2021-02-01 16:26:01 +0800

    .

--- TypeScript/chapter02/part1/src/07.属性的封装.ts
+++ TypeScript/chapter02/part1/src/07.属性的封装.ts
@@ -1,17 +1,31 @@
 (() => {
   // 定义一个表示人的类
   class Person {
+    /*
+      TS 可以在属性前添加属性的修饰符
+        public 修饰的属性可以在任意位置访问（修改），默认值
+        private 私有属性，私有属性只能在类内部进行访问（修改）
+          - 通过在类中添加方法使得私有属性可以被外部访问
+    */
     name: string;
     age: number;
     constructor(name: string, age: number) {
       this.name = name;
       this.age = age;
     }
+
+    // 定义方法，用来获取 name 属性
   }
 
   const per = new Person('孙悟空', 18);
 
   /*  
     现在的属性是在对象中设置的，属性可以任意的被修改
+    属性可以任意被修改将会导致对象中的数据变得非常不安全
   */
+
+  per.name = '猪八戒';
+  per.age = -18;
+
+  console.log(per);
 })();
