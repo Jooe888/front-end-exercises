7b21bddc630a8e980ef6d4292182623b4d6ca782
Jooe <ojooeo@gmail.com>
2021-05-13 18:01:47 +0800

    vue3

--- Vue3/vite_study/src/App.vue
+++ Vue3/vite_study/src/App.vue
@@ -1,18 +1,13 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
+<script setup>
 import HelloWorld from './components/HelloWorld.vue';
 
-@Options({
-  components: {
-    HelloWorld
-  }
-})
-export default class App extends Vue {}
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
 
 <style>
--- Vue3/vue3_study/.eslintrc.js
+++ Vue3/vue3_study/.eslintrc.js
@@ -3,10 +3,12 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/typescript/recommended'
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint'
   ],
   parserOptions: {
     ecmaVersion: 2020
@@ -15,4 +17,4 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
-}
+};
