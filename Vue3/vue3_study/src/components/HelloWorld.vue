48526c3f727ea94ccda328eceee66e43ed839abf
Jooe <ojooeo@gmail.com>
2021-05-13 18:04:48 +0800

    vue3

--- Vue3/vue3_study/src/components/HelloWorld.vue
+++ Vue3/vue3_study/src/components/HelloWorld.vue
@@ -13,16 +13,18 @@
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
           target="_blank"
           rel="noopener"
-          >typescript</a
         >
+          typescript
+        </a>
       </li>
       <li>
         <a
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
           target="_blank"
           rel="noopener"
-          >eslint</a
         >
+          eslint
+        </a>
       </li>
     </ul>
     <h3>Essential Links</h3>
@@ -38,15 +40,15 @@
       <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
       <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
       <li>
-        <a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener"
-          >vue-devtools</a
-        >
+        <a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">
+          vue-devtools
+        </a>
       </li>
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li>
-        <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener"
-          >awesome-vue</a
-        >
+        <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">
+          awesome-vue
+        </a>
       </li>
     </ul>
   </div>
